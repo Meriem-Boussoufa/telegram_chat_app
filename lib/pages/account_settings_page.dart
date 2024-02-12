d74de24840fce6b4939424b685a630c42fc67f2e
meriem <meriem@gmail.com>
2024-02-12 19:45:32 +0100

    Image Avatar, Load Data from Local Storage

--- lib/pages/account_settings_page.dart
+++ lib/pages/account_settings_page.dart
@@ -1,15 +1,161 @@
-import 'package:flutter/material.dart';
+import 'dart:io';
 
-class Settings extends StatefulWidget {
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class Settings extends StatelessWidget {
   const Settings({super.key});
 
   @override
-  State<Settings> createState() => _SettingsState();
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        iconTheme: const IconThemeData(
+          color: Colors.white,
+        ),
+        backgroundColor: Colors.lightBlue,
+        title: const Text(
+          "Account Settings",
+          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+        ),
+        centerTitle: true,
+      ),
+      body: const SettingsScreen(),
+    );
+  }
 }
 
-class _SettingsState extends State<Settings> {
+class SettingsScreen extends StatefulWidget {
+  const SettingsScreen({super.key});
+
+  @override
+  State<SettingsScreen> createState() => _SettingsScreenState();
+}
+
+class _SettingsScreenState extends State<SettingsScreen> {
+  late TextEditingController nickNameTextEditingController;
+  late TextEditingController aboutMeTextEditingController;
+
+  late SharedPreferences preferences;
+  String id = "";
+  String nickname = "";
+  String aboutMe = "";
+  String photoUrl = "";
+
+  late File imageFileAvatar;
+  bool isLoading = false;
+
+  @override
+  void initState() {
+    super.initState();
+    readDataFromLocal();
+  }
+
+  void readDataFromLocal() async {
+    preferences = await SharedPreferences.getInstance();
+    id = preferences.getString("id")!;
+    nickname = preferences.getString("nickname")!;
+    photoUrl = preferences.getString("photoUrl")!;
+    aboutMe = preferences.getString("aboutMe")!;
+
+    nickNameTextEditingController = TextEditingController(text: nickname);
+    aboutMeTextEditingController = TextEditingController(text: aboutMe);
+    setState(() {});
+  }
+
+  Future getImage() async {
+    ImagePicker imagePicker = ImagePicker();
+    XFile? newImageFile =
+        await imagePicker.pickImage(source: ImageSource.gallery);
+    // ignore: unnecessary_null_comparison
+    if (newImageFile != null) {
+      setState(() {
+        imageFileAvatar = newImageFile as File;
+        isLoading = true;
+      });
+    }
+    // uploadImageToFirestoreand Storage
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Stack(
+      children: [
+        SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                width: double.infinity,
+                margin: const EdgeInsets.all(20.0),
+                child: Center(
+                  child: Stack(
+                    children: [
+                      // ignore: unnecessary_null_comparison
+                      (imageFileAvatar == null)
+                          ? (photoUrl != "")
+                              ? Material(
+                                  // Display already existing -old image file
+                                  borderRadius: const BorderRadius.all(
+                                    Radius.circular(125.0),
+                                  ),
+                                  clipBehavior: Clip.hardEdge,
+                                  // Display already existing -old image file
+                                  child: CachedNetworkImage(
+                                    placeholder: (context, url) => Container(
+                                      child: const CircularProgressIndicator(
+                                        strokeWidth: 2.0,
+                                        valueColor:
+                                            AlwaysStoppedAnimation<Color>(
+                                                Colors.lightBlueAccent),
+                                      ),
+                                    ),
+                                    imageUrl: photoUrl,
+                                    width: 200.0,
+                                    height: 200.0,
+                                    fit: BoxFit.cover,
+                                  ),
+                                )
+                              : const Icon(
+                                  Icons.account_circle,
+                                  size: 90.0,
+                                  color: Colors.grey,
+                                )
+                          : Material(
+                              // Display The new Updated image here
+                              borderRadius: const BorderRadius.all(
+                                Radius.circular(125.0),
+                              ),
+                              clipBehavior: Clip.hardEdge,
+                              // Display The new Updated image here
+                              child: Image.file(
+                                imageFileAvatar,
+                                width: 200.0,
+                                height: 200.0,
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                      IconButton(
+                        onPressed: getImage,
+                        icon: Icon(
+                          Icons.camera_alt,
+                          size: 100.0,
+                          color: Colors.white54.withOpacity(0.3),
+                        ),
+                        padding: const EdgeInsets.all(0.0),
+                        splashColor: Colors.transparent,
+                        highlightColor: Colors.grey,
+                        iconSize: 200.0,
+                      ),
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
+        )
+      ],
+    );
   }
 }
