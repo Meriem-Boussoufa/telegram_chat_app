20d177fb88e34f244838b89bfdef94757c67b9d4
meriem <meriem@gmail.com>
2024-02-27 16:12:51 +0100

    Handle problem

--- lib/pages/chatting_page.dart
+++ lib/pages/chatting_page.dart
@@ -7,7 +7,7 @@ import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:image_picker/image_picker.dart';
-import 'package:intl/intl.dart';
+//import 'package:intl/intl.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:telegram_chat/widgets/progress_widget.dart';
 
